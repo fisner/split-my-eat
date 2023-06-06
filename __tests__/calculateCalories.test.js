1ad287d8a46d0318b188929d2c25687333be4699
Алексей Кирсанов <alexbalderlab@gmail.com>
2023-06-06 17:09:09 +0300

    add Jest tests

--- __tests__/calculateCalories.test.js
+++ __tests__/calculateCalories.test.js
@@ -2,20 +2,28 @@
 
 // 1. Проверка на корректный расчет калорий для мужчины с заданными параметрами:
 
-const result = calculateCalories(80, 180, 35, 'male');
-expect(result.calories).toEqual(3269);
+test('calculateCalories calculates correct calories for male', () => {
+  const result = calculateCalories(80, 180, 35, 'male');
+  expect(result.calories).toEqual(3269);
+});
 
-//  2. Проверка на корректный расчет калорий для женщины с заданными параметрами:
+// 2. Проверка на корректный расчет калорий для женщины с заданными параметрами:
 
-const result = calculateCalories(65, 165, 28, 'female');
-expect(result.calories).toEqual(2005);
+test('calculateCalories calculates correct calories for female', () => {
+  const result = calculateCalories(65, 165, 28, 'female');
+  expect(result.calories).toEqual(2005);
+});
 
 // 3. Проверка на корректный расчет количества белков в граммах:
 
-const result = calculateCalories(75, 170, 32, 'male');
-expect(result.protein).toEqual(165);
+test('calculateCalories calculates correct protein in grams', () => {
+  const result = calculateCalories(75, 170, 32, 'male');
+  expect(result.protein).toEqual(165);
+});
 
 // 4. Проверка на корректный расчет соотношения белков, жиров и углеводов:
 
-const result = calculateCalories(70, 175, 30, 'male');
-expect(result.macroRatio).toEqual('22-28-50');
+test('calculateCalories calculates correct macro ratio', () => {
+  const result = calculateCalories(70, 175, 30, 'male');
+  expect(result.macroRatio).toEqual('22-28-50');
+});
