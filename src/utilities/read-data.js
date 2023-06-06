bc8af075d944102edaf6141299b58ccc5211e23c
Artem Terenin <artemterenin@gmail.com>
2023-06-06 22:22:44 +0300

    change path format

--- src/utilities/read-data.js
+++ src/utilities/read-data.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
+import path from 'path';
 
 const getFoodData = () => {
-  const dataPath = `${process.cwd()}/data/food.json`;
+  const dataPath = path.resolve('./data/food.json');
   return JSON.parse(fs.readFileSync(dataPath, 'utf8'), 2);
 };
 
