88272a68c98384334ed1562a13aa9f2378a7b954
Artem Terenin <artemterenin@gmail.com>
2023-06-08 16:37:30 +0300

    update formatter tests

--- __tests__/formatter.test.js
+++ __tests__/formatter.test.js
@@ -2,11 +2,13 @@ import fs from 'fs';
 import path from 'path';
 import formatter from '../src/formatter.js';
 
-const testDataPath = path.resolve('./__fixtures__/formatter-test-data.json');
-const testData = JSON.parse(fs.readFileSync(testDataPath, 'utf8'), 2);
+const testCaloriesDataPath = path.resolve('./__fixtures__/formatter-test-caloriesData.json');
+const testCaloriesData = JSON.parse(fs.readFileSync(testCaloriesDataPath, 'utf8'), 2);
+const testRationDataPath = path.resolve('./__fixtures__/formatter-test-rationData.json');
+const testRationData = JSON.parse(fs.readFileSync(testRationDataPath, 'utf8'), 2);
 const expectedDataPath = path.resolve('./__fixtures__/formatter-expected-result.txt');
 const expectedData = fs.readFileSync(expectedDataPath, 'utf8');
 
 test('output format', () => {
-  expect(formatter(testData)).toEqual(expectedData);
+  expect(formatter('male', 33, 178, 82, testCaloriesData, testRationData)).toEqual(expectedData);
 });
