0b12fbe73faf1fa840e6c3120bdce1d235ce12c8
Artem Terenin <artemterenin@gmail.com>
2023-06-16 00:19:05 +0300

    refactoring

--- src/calculateRation.js
+++ src/calculateRation.js
@@ -1,34 +1,33 @@
 const getRandomKey = (length) => Math.floor(Math.random() * length);
 
-const foodCount = (totalMacro, foodMacro, gramsPerFood) => Math.round(
-  (totalMacro / foodMacro) * gramsPerFood,
-);
-
 const getFoodForDay = (calories, macro, foodData) => {
   const foodList = Object.keys(foodData[macro]);
   const food = foodList[getRandomKey(foodList.length)];
   const gramsPerFood = 100;
-  const foodAmount = foodCount(calories[macro], foodData[macro][food][macro], gramsPerFood);
+
+  const totalMacro = calories[macro];
+  const foodMacro = foodData[macro][food][macro];
+  const foodAmount = Math.round((totalMacro / foodMacro) * gramsPerFood);
 
   return { [food]: foodAmount };
 };
 
-const calculateRation = (calories, foodData, count = 0, acc = {}) => {
-  if (count > 6) {
+const calculateRation = (calories, foodData, days = 7, count = 1, acc = {}) => {
+  if (count > days) {
     return acc;
   }
 
   const [proteinFood, fatFood, carbsFood] = ['protein', 'fat', 'carbs']
     .map((macro) => getFoodForDay(calories, macro, foodData));
   const dayData = {
-    [count]: {
+    [count - 1]: {
       proteinFood,
       fatFood,
       carbsFood,
     },
   };
 
-  return calculateRation(calories, foodData, count + 1, { ...acc, ...dayData });
+  return calculateRation(calories, foodData, days, count + 1, { ...acc, ...dayData });
 };
 
 export default calculateRation;
