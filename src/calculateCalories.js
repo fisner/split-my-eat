42c6c7dee6a86fbaddba3e1aeb04d4825666ff90
Artem Terenin <artemterenin@gmail.com>
2023-06-21 11:00:39 +0300

    refactoring

--- src/calculateCalories.js
+++ src/calculateCalories.js
@@ -1,3 +1,58 @@
+const activityData = {
+  'no activity': {
+    activityMultiplier: 1.2,
+    macroRatio: {
+      proteinRatio: 20,
+      fatRatio: 30,
+      carbsRatio: 50,
+    },
+  },
+  '1-3 light activities per week': {
+    activityMultiplier: 1.375,
+    macroRatio: {
+      proteinRatio: 20,
+      fatRatio: 30,
+      carbsRatio: 50,
+    },
+  },
+  '3-5 moderate activities per week': {
+    activityMultiplier: 1.55,
+    macroRatio: {
+      proteinRatio: 20,
+      fatRatio: 30,
+      carbsRatio: 50,
+    },
+  },
+  '5-6 moderate activities per week': {
+    activityMultiplier: 1.725,
+    macroRatio: {
+      proteinRatio: 20,
+      fatRatio: 30,
+      carbsRatio: 50,
+    },
+  },
+  '7 vigorous activities per week': {
+    activityMultiplier: 1.9,
+    macroRatio: {
+      proteinRatio: 20,
+      fatRatio: 30,
+      carbsRatio: 50,
+    },
+  },
+};
+
+const formulasData = {
+  'Mifflin-St Jeor': {
+    weightCoefficient: 10,
+    heightCoefficient: 6.25,
+    ageCoefficient: 5,
+    genderCoefficient: {
+      male: 5,
+      female: -161,
+    },
+  },
+};
+
 const getMacrosAmount = (calories, macroRatio, caloriesInMacro) => (
   Math.round((calories * macroRatio) / 100 / caloriesInMacro)
 );
@@ -16,66 +71,11 @@ const calculateMacros = (calories, { proteinRatio, fatRatio, carbsRatio }) => {
 };
 
 const calculateCalories = (gender, age, height, weight, physicalActivity, formula = 'Mifflin-St Jeor') => {
-  const activityData = {
-    'no activity': {
-      activityMultiplier: 1.2,
-      macroRatio: {
-        proteinRatio: 20,
-        fatRatio: 30,
-        carbsRatio: 50,
-      },
-    },
-    '1-3 light activities per week': {
-      activityMultiplier: 1.375,
-      macroRatio: {
-        proteinRatio: 20,
-        fatRatio: 30,
-        carbsRatio: 50,
-      },
-    },
-    '3-5 moderate activities per week': {
-      activityMultiplier: 1.55,
-      macroRatio: {
-        proteinRatio: 20,
-        fatRatio: 30,
-        carbsRatio: 50,
-      },
-    },
-    '5-6 moderate activities per week': {
-      activityMultiplier: 1.725,
-      macroRatio: {
-        proteinRatio: 20,
-        fatRatio: 30,
-        carbsRatio: 50,
-      },
-    },
-    '7 vigorous activities per week': {
-      activityMultiplier: 1.9,
-      macroRatio: {
-        proteinRatio: 20,
-        fatRatio: 30,
-        carbsRatio: 50,
-      },
-    },
-  };
-
-  const formulas = {
-    'Mifflin-St Jeor': {
-      weightCoefficient: 10,
-      heightCoefficient: 6.25,
-      ageCoefficient: 5,
-      genderCoefficient: {
-        male: 5,
-        female: -161,
-      },
-    },
-  };
-
   const calories = Math.round((
-    (formulas[formula].weightCoefficient * weight)
-  + (formulas[formula].heightCoefficient * height)
-  - (formulas[formula].ageCoefficient * age)
-  + formulas[formula].genderCoefficient[gender])
+    (formulasData[formula].weightCoefficient * weight)
+  + (formulasData[formula].heightCoefficient * height)
+  - (formulasData[formula].ageCoefficient * age)
+  + formulasData[formula].genderCoefficient[gender])
   * activityData[physicalActivity].activityMultiplier);
 
   const {
