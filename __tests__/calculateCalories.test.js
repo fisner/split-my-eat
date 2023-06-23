439c1e8a2d5764cb384dd61e44d06d6bbf735be7
Artem Terenin <artemterenin@gmail.com>
2023-06-23 08:32:11 +0300

    update tests

--- __tests__/calculateCalories.test.js
+++ __tests__/calculateCalories.test.js
@@ -4,10 +4,10 @@ test('calculateCalories returns correct values for male', () => {
   const result = calculateCalories('male', 25, 175, 75, 'no activity', 'Mifflin-St Jeor');
   const testVal = {
     calories: 2069,
-    protein: 103,
+    protein: 78,
     fat: 69,
-    carbs: 259,
-    macroRatio: '20% 30% 50%',
+    carbs: 284,
+    macroRatio: '15% 30% 55%',
   };
   expect(result).toEqual(testVal);
 });
